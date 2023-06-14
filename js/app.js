2ba7e6be8ab902ad456a1d833ad2052ac2f95c59
Nguyễn Văn Lộc <88890670+vitamin112@users.noreply.github.com>
2023-06-14 20:44:53 +0700

    day 3

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ let body = document.body;
 let profile = document.querySelector(".header .flex .profile");
 let sideBar = document.querySelector(".side-bar");
 let searchForm = document.querySelector(".header .flex .search-form");
+let darkMode = document.querySelector("#toggle-btn");
 
 document.querySelector("#user-btn").onclick = () => {
   profile.classList.toggle("active");
@@ -25,4 +26,25 @@ document.querySelector("#menu-btn").onclick = () => {
 document.querySelector(".close-side-bar").onclick = () => {
   body.classList.remove("active");
   sideBar.classList.remove("active");
+};
+
+function enableDarkMode() {
+  body.classList.add("dark");
+  localStorage.setItem("darkMode", "true");
+  darkMode.classList.replace("fa-sun", "fa-moon");
+}
+function disableDarkMode() {
+  body.classList.remove("dark");
+  localStorage.setItem("darkMode", "false");
+  darkMode.classList.replace("fa-moon", "fa-sun");
+}
+if (localStorage.getItem("darkMode") == "true") {
+  enableDarkMode();
+}
+darkMode.onclick = () => {
+  if (localStorage.getItem("darkMode") == "true") {
+    disableDarkMode();
+  } else {
+    enableDarkMode();
+  }
 };
